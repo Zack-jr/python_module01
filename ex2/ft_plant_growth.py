5f4154f9cca37e6a5ee8ca4e4cf04289ecd858a6
Zakaria Labib-Girard <girard.zack04@gmail.com>
2026-01-26 20:24:34 +0100

    update

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -1,20 +1,37 @@
 class plant():
-    def __init__(self, name, height, age):
-        self.name = name
-        self.height = height
-        self.age = age
+    def __init__(self, plant_name, plant_height, plant_age):
+        self.plant_name = plant_name
+        self.plant_height = plant_height
+        self.plant_age = plant_age
+    def grow(self):
+        self.plant_height += 1
+    def age(self):
+        self.plant_age += 1
+    def get_info(self):
+        print(f'{self.plant_name}: {self.plant_height}cm, {self.plant_age} days old')
 
-def grow()
+
 first = plant('Rose', 25, 30)
 second = plant('Sunflower', 80, 45)
 third = plant('Cactus', 15, 120)
 
-print('=== Garden Plant Registery ===')
-print(f'{first.name}: {first.height}cm, {first.age} days old')
-print(f'{second.name}: {second.height}cm, {second.age} days old')
-print(f'{third.name}: {third.height}cm, {third.age} days old')
+i = 1
+while i <= 7:
+    print(f'=== Day {i} ===')
+    first.get_info()
+    second.get_info()
+    third.get_info()
+    first.grow()
+    second.grow()
+    third.grow()
+    first.age()
+    second.age()
+    third.age()
+    i+= 1
 
-# almost like structs but need to define with class and use __init__ to create the variables
-# use a "self" variable in the definition to store the data
-# and assign values
-# access values like structs
+
+
+print('Growth this week: +6cm')
+# same as previous but adding new methods 
+# methods are basically functions within a class
+# get the infos, grow and age the plants for each day of the week.
